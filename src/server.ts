77830e410fa06b4c3445147c436af4d76bb45151
asafedainez <asafe.sousa@gmail.com>
2022-07-17 15:19:45 -0300

    feat(route): implementado endpoint POST /login

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import 'express-async-errors';
 import errorMiddleware from './middleware/error.middleware';
+import routes from './routes';
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const { NODE_PORT } = process.env;
 const server = express();
 
 server.use(express.json());
+
+server.use(routes)
 
 server.use(errorMiddleware);
 
