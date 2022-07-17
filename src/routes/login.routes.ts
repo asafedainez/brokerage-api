99757d87c3d4ee1c896ba626e0e8b12d10be9ee8
asafedainez <asafe.sousa@gmail.com>
2022-07-17 15:18:55 -0300

    feat(route): implementado rota de login

---
+++ src/routes/login.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import LoginController from "../controllers/Login.controller";
+import LoginMiddleware from "../middleware/login.middleware";
+
+const loginController = new LoginController();
+
+const loginRouter = Router();
+
+loginRouter.post('/login', LoginMiddleware, loginController.login);
+
+export default loginRouter;
