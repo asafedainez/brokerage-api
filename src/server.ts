af1d7210a073feaf45a8f4d827a621a3ea9b1846
asafedainez <asafe.sousa@gmail.com>
2022-07-19 17:00:22 -0300

    refactor: criado um arquivo app.ts para inicializar o express e o arquivo server.ts é só para subir o servidor
    Feito isso para facilitar a implementação de testes futuros

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,10 @@
 import dotenv from 'dotenv';
-import express from 'express';
-import 'express-async-errors';
-import errorMiddleware from './middleware/error.middleware';
-import routes from './routes';
+import app from './app';
 
 dotenv.config();
 
 const { NODE_PORT } = process.env;
-const server = express();
 
-server.use(express.json());
-
-server.use(routes)
-
-server.use(errorMiddleware);
-
-server.listen(NODE_PORT, () => {
+app.listen(NODE_PORT, () => {
   console.log(`Server is running on port ${NODE_PORT}`);
 });
