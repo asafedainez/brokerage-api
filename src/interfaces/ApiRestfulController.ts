c78f855d42a45c05aa3e47a8a4fa9ab90d4318fb
asafedainez <asafe.sousa@gmail.com>
2022-07-20 11:36:37 -0300

    feat(interface): criada interface para API RESTful Controller

---
+++ src/interfaces/ApiRestfulController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+
+export default interface IApiRestfulController {
+  getAll(req: Request, res: Response): Promise<Response>;
+  getById(req: Request, res: Response): Promise<Response>;
+  create(req: Request, res: Response): Promise<Response>;
+  update(req: Request, res: Response): Promise<Response>;
+  remove(req: Request, res: Response): Promise<Response>;
+  getAccountBalance(req: Request, res: Response): Promise<Response>;
+  accountDeposit(req: Request, res: Response): Promise<Response>;
+  accountWithdraw(req: Request, res: Response): Promise<Response>;
+  getAssets(req: Request, res: Response): Promise<Response>;
+}
