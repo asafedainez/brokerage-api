2337205179ef3fdc21c8b5c66cf119ffa615397d
asafedainez <asafe.sousa@gmail.com>
2022-07-20 14:50:22 -0300

    refactor(controller): transformado método de userService e getUserId em estáticos por não estarem funcionando como métodos padrões

--- src/controllers/User.controller.ts
+++ src/controllers/User.controller.ts
@@ -6,85 +6,13 @@ import Auth from '../utils/Auth';
 import HttpException from '../utils/HttpException';
 
 export default class UserController implements IApiRestfulController {
-  protected userService: UserService;
+  protected static userService = new UserService();
 
-  constructor() {
-    this.userService = new UserService();
-  }
+  // constructor() {
+  //   this.userService = new UserService();
+  // }
 
-  async getById(req: Request, res: Response): Promise<Response> {
-    const userId = this.getUserId(req);
-    const user = await this.userService.getById(userId);
-
-    return res.status(StatusCodes.OK).json({ user });
-  }
-
-  async create(req: Request, res: Response): Promise<Response> {
-    const { cpf, name, email, password } = req.body;
-
-    const token = await this.userService.create({ cpf, name, email, password });
-
-    return res.status(StatusCodes.CREATED).json({ token });
-  }
-
-  async update(req: Request, res: Response): Promise<Response> {
-    const userId = this.getUserId(req);
-    const { name, email, cpf } = req.body;
-
-    const userUpdated = await this.userService.update(userId, {
-      name,
-      email,
-      cpf,
-    });
-
-    return res.status(StatusCodes.OK).json({ user: userUpdated });
-  }
-
-  async remove(req: Request, res: Response): Promise<Response> {
-    const userId = this.getUserId(req);
-
-    await this.userService.remove(userId);
-
-    return res.status(StatusCodes.NO_CONTENT).json({ message: 'User removed' });
-  }
-
-  async getAccountBalance(req: Request, res: Response): Promise<Response> {
-    const userId = this.getUserId(req);
-
-    const balance = await this.userService.getAccountBalance(userId);
-
-    return res.status(StatusCodes.OK).json({ balance });
-  }
-
-  async accountDeposit(req: Request, res: Response): Promise<Response> {
-    const userId = this.getUserId(req);
-
-    const { value } = req.body;
-
-    const deposit = await this.userService.accountDeposit(userId, value);
-
-    return res.status(StatusCodes.OK).json({ deposit });
-  }
-
-  async accountWithdraw(req: Request, res: Response): Promise<Response> {
-    const userId = this.getUserId(req);
-
-    const { value } = req.body;
-
-    const withdraw = await this.userService.accountWithdraw(userId, value);
-
-    return res.status(StatusCodes.OK).json({ withdraw });
-  }
-
-  async getAssets(req: Request, res: Response): Promise<Response> {
-    const userId = this.getUserId(req);
-
-    const assets = await this.userService.getAssets(userId);
-
-    return res.status(StatusCodes.OK).json({ assets });
-  }
-
-  private getUserId(req: Request): string {
+  private static getUserId(req: Request): string {
     const userToken = req.headers.authorization?.split('Bearer ')[1];
 
     if (!userToken) {
@@ -94,5 +22,89 @@ export default class UserController implements IApiRestfulController {
     const userId = Auth.verify(userToken).sub;
 
     return userId;
+  }
+
+  async getById(req: Request, res: Response): Promise<Response> {
+    const userId = UserController.getUserId(req);
+
+    const user = await UserController.userService.getById(userId);
+
+    return res.status(StatusCodes.OK).json({ user });
+  }
+
+  async create(req: Request, res: Response): Promise<Response> {
+    const { cpf, name, email, password } = req.body;
+
+    const token = await UserController.userService.create({
+      cpf,
+      name,
+      email,
+      password,
+    });
+
+    return res.status(StatusCodes.CREATED).json({ token });
+  }
+
+  async update(req: Request, res: Response): Promise<Response> {
+    const userId = UserController.getUserId(req);
+    const { name, email, cpf } = req.body;
+
+    const userUpdated = await UserController.userService.update(userId, {
+      name,
+      email,
+      cpf,
+    });
+
+    return res.status(StatusCodes.OK).json({ user: userUpdated });
+  }
+
+  async remove(req: Request, res: Response): Promise<Response> {
+    const userId = UserController.getUserId(req);
+
+    await UserController.userService.remove(userId);
+
+    return res.status(StatusCodes.NO_CONTENT).json({ message: 'User removed' });
+  }
+
+  async getAccountBalance(req: Request, res: Response): Promise<Response> {
+    const userId = UserController.getUserId(req);
+
+    const balance = await UserController.userService.getAccountBalance(userId);
+
+    return res.status(StatusCodes.OK).json({ balance });
+  }
+
+  async accountDeposit(req: Request, res: Response): Promise<Response> {
+    const userId = UserController.getUserId(req);
+
+    const { value } = req.body;
+
+    const deposit = await UserController.userService.accountDeposit(
+      userId,
+      value
+    );
+
+    return res.status(StatusCodes.OK).json({ deposit });
+  }
+
+  async accountWithdraw(req: Request, res: Response): Promise<Response> {
+    const userId = UserController.getUserId(req);
+
+    const { value } = req.body;
+
+    const withdraw = await UserController.userService.accountWithdraw(
+      userId,
+      value
+    );
+
+    return res.status(StatusCodes.OK).json({ withdraw });
+  }
+
+  async getAssets(req: Request, res: Response): Promise<Response> {
+    const userId = UserController.getUserId(req);
+
+    const assets = await UserController.userService.getAssets(userId);
+
+    return res.status(StatusCodes.OK).json({ assets });
   }
 }
