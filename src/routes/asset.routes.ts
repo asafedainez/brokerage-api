cd35a70fad7cb6772895e333cfee365230b72b5f
asafedainez <asafe.sousa@gmail.com>
2022-07-21 15:44:07 -0300

    feat(route): implementado assetMiddleware nas rotas POST e PUT de /assets

--- src/routes/asset.routes.ts
+++ src/routes/asset.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import AssetController from '../controllers/Asset.controller';
+import assetMiddleware from '../middleware/asset.middleware';
 import tokenMiddleware from '../middleware/token.middleware';
 
 const assetController = new AssetController();
@@ -11,7 +12,7 @@ assetRouter.post('assets/sell', tokenMiddleware, assetController.sellAsset);
 
 assetRouter.get('/assets/:id', assetController.getById);
 assetRouter.get('/assets', assetController.getAll);
-assetRouter.post('/assets', assetController.create);
-assetRouter.put('/assets/:id', assetController.update);
+assetRouter.post('/assets', assetMiddleware, assetController.create);
+assetRouter.put('/assets/:id', assetMiddleware, assetController.update);
 
 export default assetRouter;
