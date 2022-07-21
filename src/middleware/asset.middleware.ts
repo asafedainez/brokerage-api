884e5bffc7a4cf66facb9baa74d2a76b7f45e9ae
asafedainez <asafe.sousa@gmail.com>
2022-07-21 15:43:21 -0300

    feat(middleware): criado middleware para verificar dados ao criar e atualizar um asset

---
+++ src/middleware/asset.middleware.ts
@@ -0,0 +1,24 @@
+import Joi from 'joi';
+import { NextFunction, Request, Response } from 'express';
+import HttpException from '../utils/HttpException';
+import { StatusCodes } from 'http-status-codes';
+
+const assetSchema = Joi.object().keys({
+  assetName: Joi.string().length(5).required(),
+  value: Joi.number().min(0).required(),
+  quantity: Joi.number().min(0).required(),
+});
+
+export default function assetMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { error } = assetSchema.validate(req.body);
+
+  if (error) {
+    throw new HttpException(StatusCodes.BAD_REQUEST, error.details[0].message);
+  }
+
+  next();
+}
