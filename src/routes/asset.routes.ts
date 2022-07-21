06434b81b0db5612232efeb85be46d3b69e56fb7
asafedainez <asafe.sousa@gmail.com>
2022-07-21 15:52:54 -0300

    feat(route): implementado operateAssetMiddleware nas rotas de compra e venda de ativos

--- src/routes/asset.routes.ts
+++ src/routes/asset.routes.ts
@@ -2,13 +2,24 @@ import { Router } from 'express';
 import AssetController from '../controllers/Asset.controller';
 import assetMiddleware from '../middleware/asset.middleware';
 import tokenMiddleware from '../middleware/token.middleware';
+import operateAssetMiddleware from '../middleware/operateAsset.middleware';
 
 const assetController = new AssetController();
 
 const assetRouter = Router();
 
-assetRouter.post('assets/buy', tokenMiddleware, assetController.buyAsset);
-assetRouter.post('assets/sell', tokenMiddleware, assetController.sellAsset);
+assetRouter.post(
+  'assets/buy',
+  tokenMiddleware,
+  operateAssetMiddleware,
+  assetController.buyAsset
+);
+assetRouter.post(
+  'assets/sell',
+  tokenMiddleware,
+  operateAssetMiddleware,
+  assetController.sellAsset
+);
 
 assetRouter.get('/assets/:id', assetController.getById);
 assetRouter.get('/assets', assetController.getAll);
