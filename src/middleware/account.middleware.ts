2982211f5c9fc49005753a9f8c68ad420c6364bc
asafedainez <asafe.sousa@gmail.com>
2022-07-22 17:59:13 -0300

    feat(middleware): criado middleware para verificar dados das requisições de conta

---
+++ src/middleware/account.middleware.ts
@@ -0,0 +1,22 @@
+import Joi from 'joi';
+import { NextFunction, Request, Response } from 'express';
+import HttpException from '../utils/HttpException';
+import { StatusCodes } from 'http-status-codes';
+
+const accountSchema = Joi.object().keys({
+  value: Joi.number().min(0).required(),
+});
+
+export default function accountMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { error } = accountSchema.validate(req.body);
+
+  if (error) {
+    throw new HttpException(StatusCodes.BAD_REQUEST, error.details[0].message);
+  }
+
+  next();
+}
