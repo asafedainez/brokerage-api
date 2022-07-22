e37c57967a2c6ac34b545927768b29312058bd64
asafedainez <asafe.sousa@gmail.com>
2022-07-22 16:03:21 -0300

    test(asset): feito teste para verificar se é possível comprar um ativo que não existe

--- src/__tests__/Assets.test.ts
+++ src/__tests__/Assets.test.ts
@@ -2,9 +2,14 @@ import request from 'supertest';
 import app from '../app';
 import { prismaMock } from './prismaMock';
 import AssetsService from '../services/Assets.service';
+import IAsset from '../interfaces/Asset';
+import { StatusCodes } from 'http-status-codes';
 
 describe('Verifica venda de ativos', () => {
-  test('Verifica se é possível comprar um ativo sem saldo na conta do usuário', async () => {
+  let tokenUser: string;
+  let allAssets: IAsset[];
+
+  beforeAll(async () => {
     const reqToken = await request(app).post('/login').send({
       cpf: '12345678902',
       password: '12345678',
@@ -12,10 +17,12 @@ describe('Verifica venda de ativos', () => {
 
     const reqAllAssets = await request(app).get('/asset');
 
-    const allAssets = reqAllAssets.body;
+    allAssets = reqAllAssets.body;
 
-    const tokenUser = reqToken.body.token;
+    tokenUser = reqToken.body.token;
+  });
 
+  test('Verifica se é possível comprar um ativo sem saldo na conta do usuário', async () => {
     const reqBuy = await request(app)
       .post('/asset/buy')
       .send({
@@ -24,9 +31,22 @@ describe('Verifica venda de ativos', () => {
       })
       .set('Authorization', `Bearer ${tokenUser}`);
 
-    expect(reqBuy.status).toBe(400);
+    expect(reqBuy.status).toBe(StatusCodes.BAD_REQUEST);
     expect(reqBuy.body.message).toBe(
       'Not enough funds to buy this quantity of assets'
     );
+  });
+
+  test('Verifica se é possível comprar um ativo que não existe', async () => {
+    const reqBuy = await request(app)
+      .post('/asset/buy')
+      .send({
+        idAsset: 'xablau',
+        quantity: 15,
+      })
+      .set('Authorization', `Bearer ${tokenUser}`);
+
+    expect(reqBuy.status).toBe(StatusCodes.NOT_FOUND);
+    expect(reqBuy.body.message).toBe('Asset not found');
   });
 });
