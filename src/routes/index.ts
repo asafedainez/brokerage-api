7c47da2b973a9218e526340447e018dee146138b
asafedainez <asafe.sousa@gmail.com>
2022-07-24 19:15:18 -0300

    feat(route): adicionando dado no método GET da rota / para retornar alguns dados do projeto

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import assetRouter from './asset.routes';
 import loginRouter from './login.routes';
 import userRouter from './user.routes';
@@ -10,5 +10,11 @@ routes.use(loginRouter);
 routes.use(userRouter);
 routes.use(assetRouter);
 routes.use(accountRouter);
+
+routes.get('/', (req: Request, res: Response) => {
+  res.send(
+    '<h1>Brokerage API</h1><ul><li><a href="https://brokerageapi.herokuapp.com/docs" target="_blank">Documentação</a></li><li><a href="https://github.com/asafedainez/brokerage-api.git" target="_blank">Repositório GitHub</a></li></ul>'
+  );
+});
 
 export default routes;
