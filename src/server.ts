c0df4972fe398c7c21c3993fb8d27a02632311cf
asafedainez <asafe.sousa@gmail.com>
2022-07-24 18:53:08 -0300

    feat(route): criado rota /docs para Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import dotenv from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
 import app from './app';
+import swaggerConfig from './swagger';
 
 dotenv.config();
 
 const { PORT } = process.env;
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
